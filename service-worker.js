d9e61121d8b0a60a71d59df998704c460fc646c9
TranTam <tamvaa1306@gmail.com>
2023-11-09 16:37:47 +0700

    context menu create and trigger

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,17 @@
-console.log("Background");
+chrome.contextMenus.create({
+  id: "foo",
+  title: "Click me",
+  contexts: ["action"],
+});
+
+function contextClick(info, tab) {
+  const { menuItemId } = info;
+
+  console.log(menuItemId);
+  if (menuItemId === "foo") {
+    // do something
+    console.log("Clicked from context menu");
+  }
+}
+
+chrome.contextMenus.onClicked.addListener(contextClick);
